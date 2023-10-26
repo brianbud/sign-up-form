720d1a4558f58a7919b04905f826d1fe65a8f39a
Brian Bud <hello@brianbud.com>
2023-10-26 19:48:50 -0400

    check if confirm password matches pw value

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const btn = document.querySelector("button");
+const password = document.querySelector("#pw");
+const confirmPw = document.querySelector("#pw-confirm");
+
+confirmPw.addEventListener("input", () => {
+  if (password.value === confirmPw.value) {
+    console.log("pw matches");
+  } else {
+    console.log(" not matching");
+  }
+});
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
